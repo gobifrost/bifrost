6967d400af7c270a57e7926b99bac50c99a050dc
Jack Musick <jack@gocovi.com>
2026-09-18 01:14:17 -0400

    Shorten merge revision id to fit varchar(32)

--- api/alembic/versions/20260918_merge_plat_prof_heads.py
+++ api/alembic/versions/20260918_merge_plat_prof_heads.py
@@ -1,6 +1,6 @@
 """merge platform_job_k8s_ns and profile_max_tokens heads
 
-Revision ID: 20260918_merge_platform_profile_heads
+Revision ID: 20260918_merge_plat_prof_heads
 Revises: 20260915_platform_job_k8s_ns, 20260917_profile_max_tokens
 Create Date: 2026-09-18
 
@@ -11,7 +11,7 @@ default_max_tokens work developed in parallel.
 from typing import Sequence, Union
 
 # revision identifiers, used by Alembic.
-revision: str = "20260918_merge_platform_profile_heads"
+revision: str = "20260918_merge_plat_prof_heads"
 down_revision: Union[str, Sequence[str]] = (
     "20260915_platform_job_k8s_ns",
     "20260917_profile_max_tokens",
